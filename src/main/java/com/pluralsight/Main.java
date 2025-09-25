22a380506756c013d5760f15ace3308f3de02b44
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-25 11:22:11 -0400

    option to multiply comment

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -7,14 +7,15 @@ public class Main {
     public static void main(String[] args) {
 // create a scanner
         Scanner myScanner = new Scanner(System.in);
-        System.out.print("5");
+        System.out.print(5);
         int num1 = myScanner.nextInt();
-        System.out.print("12");
+        System.out.print(12);
         System.out.println("Add");
         System.out.println("Subtract");
         System.out.println("Mulitply");
         System.out.println("Divide");
         System.out.println("Please select an Option");
+        System.out.println(60);
 
 
 
